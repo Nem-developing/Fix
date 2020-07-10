cffb7264ed3f32e7ddbdc5773bc4c90205224d24
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-11 01:25:55 +0200

    Ajout d'informations concernant le fonctionnement des BD

--- config/config.php
+++ config/config.php
@@ -1,17 +1,35 @@
 <?php
 
-/*  
- *  =========================================================================
- *  Veuillez spécifiez les informations de connection à votre base de donnée.
- * 
- * 
- *  =========================================================================
- */ 
+    /*  
+     *  =========================================================================
+     *  Veuillez spécifiez les informations de connection à votre base de donnée.
+     * 
+     *  Veuillez vous référer à la documentation !
+     *  =========================================================================
+     */ 
 
-$hotedeconnexion = "127.0.0.1"; // 127.0.0.1 = Localhost
-$basededonnee = "Fix";
-$utilisateur = "Fix-USER";
-$motdepasse = "Fix-PASSWORD";
+    $hotedeconnexion = "127.0.0.1"; // 127.0.0.1 = Localhost
+    $basededonnee = "Fix";
+    $utilisateur = "Fix-USER";
+    $motdepasse = "Fix-PASSWORD";
 
 
+    /* =========================================================================
+     * Voici comment fonctionne les tickets dans la base de donnée : 
+     * 
+     *                      Différentes options du statut :
+     * 
+     *  0 : Non-Traité
+     *  1 : En-Cour
+     *  2 : Archivé
+     * 
+     *                      Différentes options de niveau d'urgence :
+     *  
+     * 0 : Faible
+     * 1 : Normale
+     * 2 : Urgent
+     * 
+     * 
+     * =========================================================================
+     */
 ?>
