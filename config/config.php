38704f2a93da1e6929e086f9e2141c87dab80707
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-11 00:46:32 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*  
+ *  =========================================================================
+ *  Veuillez spécifiez les informations de connection à votre base de donnée.
+ * 
+ * 
+ *  =========================================================================
+ */ 
+
+$hotedeconnexion = "127.0.0.1"; // 127.0.0.1 = Localhost
+$basededonnee = "Nix";
+$utilisateur = "Nix-USER";
+$motdepasse = "Nix-PASSWORD";
+
+
+?>
