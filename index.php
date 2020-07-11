d52d093f78295562ffa5e8b70ac237b8cdf86a7e
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-11 11:03:01 +0200

    Changement de colonnes et d’Icônes.

--- index.php
+++ index.php
@@ -46,10 +46,9 @@ Projet réalisé par Nem-developing, tout droits réservés.
 
             <?php
             include './config/config.php';  // Import des informations de connexion à la base de données.
-            
             // Établissement de la connexion au serveur mysql.
             $cnx = new PDO("mysql:host=$hotedeconnexion;dbname=$basededonnee", "$utilisateur", "$motdepasse");
-            
+
             // Commande SQL permetant de récupérer la liste des tickets non-traités.
             $req = 'SELECT * FROM `tickets` where `statut` = "0";';
             ?>
@@ -62,7 +61,8 @@ Projet réalisé par Nem-developing, tout droits réservés.
                             <th scope="col">Date</th>
                             <th scope="col">Serveur</th>
                             <th scope="col">Description</th>
-                            <th scope="col">Archiver</th>
+                            <th scope="col">État</th>
+                            <th scope="col">Détails</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -71,9 +71,11 @@ Projet réalisé par Nem-developing, tout droits réservés.
                             <td>12/1/2020</td>
                             <td>Faction</td>
                             <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis feugiat condimentum. Nulla facilisi. Phasellus elit magna, cursus ac vehicula vitae, malesuada eget neque. Duis ac nulla eu dolor cursus pretium. Pellentesque vitae tincidunt dui. Aenean eget ultricies elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc dapibus ante vel lectus bibendum porttitor. Aliquam convallis congue fringilla.</td>
+                            <td>Non-Traité</td>
                             <td>
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM6 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
                             </td>
                         </tr>
@@ -82,9 +84,11 @@ Projet réalisé par Nem-developing, tout droits réservés.
                             <td>13/1/2020</td>
                             <td>Minage</td>
                             <td>Nunc diam dolor, commodo nec arcu non, facilisis luctus tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris purus massa, consectetur et fermentum id, semper ut erat. Vivamus risus nunc, semper convallis rutrum a, imperdiet vitae ipsum. Aliquam luctus erat vitae efficitur iaculis. Proin egestas tristique libero et dignissim. Cras hendrerit lorem ut purus pretium, vel fringilla quam commodo. Praesent non blandit urna. Etiam venenatis elit vel velit varius posuere.</td>
+                            <td>Non-Traité</td>
                             <td>
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM6 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
                             </td>
                         </tr>
@@ -93,9 +97,11 @@ Projet réalisé par Nem-developing, tout droits réservés.
                             <td>14/1/2020</td>
                             <td>Opprison</td>
                             <td>Mauris vel nibh pharetra, tempus sapien vitae, pretium mi. Sed at aliquam augue. Sed in justo nec turpis interdum feugiat. Nunc tincidunt pretium urna varius gravida. Cras nulla quam, pretium finibus sem eu, commodo porttitor sem. Quisque blandit efficitur dolor a sodales. Pellentesque cursus metus at mi malesuada vehicula. Donec quis sollicitudin sapien. Quisque id eros sem. Praesent dignissim ac ex nec euismod. Fusce eleifend augue eget turpis porta luctus. Phasellus dictum ligula mi, quis consequat leo sagittis in.</td>
+                            <td>En-cours</td>
                             <td>
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-archive-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM6 7a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1H6zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z"/>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                 </svg>
                             </td>
                         </tr>
