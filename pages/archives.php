583bec434eb71a5ba6a0a7ae83d22f0276b6ac64
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-11 21:31:15 +0200

    Changement lien.

--- pages/archives.php
+++ pages/archives.php
@@ -105,7 +105,7 @@ Projet réalisé par Nem-developing, tout droits réservés.
                             <td><h4>$ligne->sujetprincipal</h4><br>$ligne->description</td>
                             <td>$urgence</td>
                             <td>$etat</td>
-                            <td><a href='pages/ticket.php?id=$ligne->id'>
+                            <td><a href='../actions/desarchiver.php?id=$ligne->id'>
                                 <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-arrow-left-circle' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
                                     <path fill-rule='evenodd' d='M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/>
                                     <path fill-rule='evenodd' d='M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z'/>
