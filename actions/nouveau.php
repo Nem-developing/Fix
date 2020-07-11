7a2f17c78374da1b399b38f4847e562145e81554
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-11 15:01:04 +0200

    Ajout du fichier nouveau.php

---
+++ actions/nouveau.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+Projet réalisé par Nem-developing, tout droits réservés.
+-->
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <title>Fix - Tickets</title>
+    </head>
+    <body>
+
+        <!-- Menu -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="../index.php">Fix</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.php">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/Nem-developing/fix/">Source code</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">À propos</a>
+                    </li>
+                </ul>
+                <a class="version" href="https://github.com/nem-developing/">Nix 1.0 - Nem-Developing</a>
+            </div>
+        </nav>  
+
+
+ 
+        <div id="page">
+            
+            <?php
+            include "../config/config.php"; // Import des données de connexion.
+            date_default_timezone_set('UTC');   // On informe mysql de la zone temporelle souhaitée.
+            $sujetprincipal = $_POST['sujetprincipal'];       // On récupère les informations du formulaire précédent.
+            $description = $_POST['description'];       // On récupère les informations du formulaire précédent.
+            $urgence = $_POST['urgence'];       // On récupère les informations du formulaire précédent.
+            $date = strftime("%d/%m/%y");       // On entre la date dans la variable $date.
+            
+            
+            
+            echo "$sujetprincipal ; $description ; $urgence ; $date";
+            
+            
+            
+            ?>
+            
+            
+            
+
+        </div>
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+</html>
