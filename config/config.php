03a881adbec9cecb37e2119f7df56dfc2f13291f
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-13 21:20:29 +0200

    Ajout d'une tentative de connexion.

--- config/config.php
+++ config/config.php
@@ -32,4 +32,14 @@
      * 
      * =========================================================================
      */
+
+
+    
+    // Tentative de connexion au seveur de bases de données.
+    $connexion = mysqli_connect($hotedeconnexion, $utilisateur, $motdepasse, $basededonnee);
+    
+    // Affichage d'un message d'erreur si il y a un soucis avec l'établissement du lien entre PHP et MYSQL - MARIADB
+    if(!$connexion) {
+        echo("Problème de connexion à la base de données ! Vérifiez les informations saisies dans la configuration.");
+    }
 ?>
