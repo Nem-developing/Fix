830676801a280ee6106549d74cba37c2a50ed69a
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-14 13:02:02 +0200

    Ajout de la date de fin.

--- actions/archiver.php
+++ actions/archiver.php
@@ -50,7 +50,9 @@ Projet réalisé par Nem-developing, tout droits réservés.
             <?php
             include "../config/config.php"; // Import des données de connexion.
             $id = $_GET['id'];  // On récupère l'ID.
+            $datefin = strftime("%d/%m/%y");       // On entre la date dans la variable $datepec.
             
+          
             (int) $erreur = 0;
             
             
@@ -62,7 +64,7 @@ Projet réalisé par Nem-developing, tout droits réservés.
                 $erreur = $erreur + 1;
             }
             // Mise à jour de la la table où l'on stoque les informations du ticket.
-            if (!$mysqli->query("UPDATE `tickets` SET `etat` = '2'  WHERE `id` = '$id'")) {
+            if (!$mysqli->query("UPDATE `tickets` SET `datefin` = '$datefin', `etat` = '2'  WHERE `id` = '$id'")) {
                 echo "<div class='alert alert-danger' role='alert'> Echec lors de la création de la table serveurs ! </div>";    // Affichage de l'erreur.
                 echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
                 $erreur = $erreur + 1;
