750d4df325c3c361f046efb6e6186e71f16f1da9
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-14 12:59:30 +0200

    Ajout de la date de prise en charge.

--- actions/prise-en-charge.php
+++ actions/prise-en-charge.php
@@ -51,7 +51,8 @@ Projet réalisé par Nem-developing, tout droits réservés.
             include "../config/config.php"; // Import des données de connexion.
             $id = $_GET['id'];  // On récupère l'ID.
             $technicien = $_SESSION['utilisateur'];       // On récupère l'utilisateur dans le cookie.
-            
+            $datepec = strftime("%d/%m/%y");       // On entre la date dans la variable $datepec.
+
             (int) $erreur = 0;
             
             
@@ -63,7 +64,7 @@ Projet réalisé par Nem-developing, tout droits réservés.
                 $erreur = $erreur + 1;
             }
             // Mise à jour de la la table où l'on stoque les informations du ticket.
-            if (!$mysqli->query("UPDATE `tickets` SET `etat` = '1', `technicien` = '$technicien'  WHERE `id` = '$id'")) {
+            if (!$mysqli->query("UPDATE `tickets` SET `datepec` = '$datepec', `etat` = '1', `technicien` = '$technicien'  WHERE `id` = '$id'")) {
                 echo "<div class='alert alert-danger' role='alert'> Echec lors de la création de la table serveurs ! </div>";    // Affichage de l'erreur.
                 echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
                 $erreur = $erreur + 1;
