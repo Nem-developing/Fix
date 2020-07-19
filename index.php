7ef3cf2b87500890c6a39ad331ead96ebcd71eea
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-19 17:51:56 +0200

    Changement de couleur.

--- index.php
+++ index.php
@@ -85,26 +85,26 @@ Projet réalisé par Nem-developing, tout droits réservés.
                             // Changement de l'INT en texte.
                             switch ($ligne->urgence) {
                                 case 0:
-                                    $urgence = "<strong><span class='bg-success text-body'>Faible</span></strong>";
+                                    $urgence = "<strong><span class='text-success'>Faible</span></strong>";
                                     break;
                                 case 1:
-                                    $urgence = "<strong><span class='bg-warning text-body'>Normal</span></strong>";
+                                    $urgence = "<strong><span class='text-warning'>Normal</span></strong>";
                                     break;
                                 case 2:
-                                    $urgence = "<strong><span class='bg-danger text-body'>Urgent</span></strong>";
+                                    $urgence = "<strong><span class='text-danger'>Urgent</span></strong>";
                                     break;
                             }
 
                             // Changement de l'INT en texte.
                             switch ($ligne->etat) {
                                 case 0:
-                                    $etat = "<span class='bg-danger'>Non-Traité</span>";
+                                    $etat = "<strong><span class='text-danger'>Non-Traité</span></strong>";
                                     break;
                                 case 1:
-                                    $etat = "<span class='bg-success'>En-cours</span>";
+                                    $etat = "<strong><span class='text-success'>En-cours</span></strong>";
                                     break;
                                 case 2:
-                                    $etat= "<span class='bg-info'>Archivé</span>";
+                                    $etat= "<strong><span class='text-info'>Archivé</span></strong>";
                                     break;
                             }
 
