1172481b9a57d874d1ac3b93a0281aedec17d2f8
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-07-19 12:48:28 +0200

    Ajout du commentaire;

--- actions/desarchiver.php
+++ actions/desarchiver.php
@@ -63,7 +63,7 @@ Projet réalisé par Nem-developing, tout droits réservés.
                 $erreur = $erreur + 1;
             }
             // Mise à jour de la la table où l'on stoque les informations du ticket.
-            if (!$mysqli->query("UPDATE `tickets` SET `etat` = '0', `heure` = '$heure',`date` = '$date',`datepec` = 'N/A', `datefin` = 'N/A',`heurepec` = 'N/A', `heurefin` = 'N/A', `technicien` = 'N/A'  WHERE `id` = '$id'")) {
+            if (!$mysqli->query("UPDATE `tickets` SET `etat` = '0', `heure` = '$heure',`date` = '$date',`datepec` = 'N/A', `datefin` = 'N/A',`heurepec` = 'N/A', `heurefin` = 'N/A', `technicien` = 'N/A', `commentaire` = 'N/A' WHERE `id` = '$id'")) {
                 echo "<div class='alert alert-danger' role='alert'> Echec lors de la création de la table serveurs ! </div>";    // Affichage de l'erreur.
                 echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
                 $erreur = $erreur + 1;
@@ -87,4 +87,4 @@ Projet réalisé par Nem-developing, tout droits réservés.
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
-</html>
+</html> 
