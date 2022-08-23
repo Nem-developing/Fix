d3a269dc3efce379d79ffc030badbafec09d83eb
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2022-08-23 02:20:29 +0200

    Update verif_licence_enfants.php

--- includes/verif_licence_enfants.php
+++ includes/verif_licence_enfants.php
@@ -1,27 +1,25 @@
 <?php
 
-try {
-    include '../config/config.php';  // Import des informations de connexion à la base de données.
-    // Établissement de la connexion au serveur mysql.
+    include '../config/config.php';  
     $cnx = new PDO("mysql:host=$hotedeconnexion;dbname=$basededonnee", "$utilisateur", "$motdepasse");
-    // Commande récupérant l'utilisateur connecté.
     $req = 'SELECT * FROM options;';
-    // Envoie au serveur la commande via le biais des informations de connexion.
     $res = $cnx->query($req);
     $cpt = 0;
-
-    // Boucle tant qu'il y a de lignes corespondantes à la requette une.
+    
+    $ligne="";
+   
     while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
         $licenceactuelle = $ligne->licence;
         $cpt = $cpt + 1;
     }
+    
 
     if (!$licenceactuelle) {
         header('Location: ../pages/parametres.php?erreur=licencemanquante');
         exit();
     }
     
-    // Vérification si à jour
+    // Vérification update
     $response = file_get_contents("https://api.nehemiebarkia.fr/fix/?verify=$licenceactuelle");
     $obj = json_decode($response);
     $licence_statut = $obj->{'Statut'};
@@ -29,8 +27,3 @@ try {
         header('Location: ../pages/parametres.php?erreur=licencemanquante');
         exit();
     }
-} catch (Exception $exc) {
-    echo $exc->getTraceAsString();
-}
-
-    
