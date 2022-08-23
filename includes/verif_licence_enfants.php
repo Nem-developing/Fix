65419aeac04795e1f796e47152587cc17d927523
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2022-08-23 02:45:28 +0200

    Correction Bug

--- includes/verif_licence_enfants.php
+++ includes/verif_licence_enfants.php
@@ -5,21 +5,20 @@
     $req = 'SELECT * FROM options;';
     $res = $cnx->query($req);
     $cpt = 0;
-    
-    $ligne="";
-   
-    while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
-        $licenceactuelle = $ligne->licence;
-        $cpt = $cpt + 1;
-    }
-    
-
-    if (!$licenceactuelle) {
+        
+    if (!$ligne){
         header('Location: ../pages/parametres.php?erreur=licencemanquante');
         exit();
+    } else {
+        while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
+            $licenceactuelle = $ligne->licence;
+            $cpt = $cpt + 1;
+        }   
     }
     
-    // Vérification update
+    
+    
+    // Vérification si à jour
     $response = file_get_contents("https://api.nehemiebarkia.fr/fix/?verify=$licenceactuelle");
     $obj = json_decode($response);
     $licence_statut = $obj->{'Statut'};
