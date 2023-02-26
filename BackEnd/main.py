bf7688b2eee47e8db2b3dbf05d936d4aea079b2c
Nem-developing <nem05yt@gmail.com>
2023-02-27 00:58:46 +0100

    :tada: (Backend) : Create route witch get a ticket !

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -180,7 +180,66 @@ async def web_get_all_tikets(request):
         data = {"Error": False, "Tickets": tickets}
 
     except:
-        data = {"Error": True, "DB error": True, "DB Message" : req.ERROR_MSG}
+        data = {"Error": True, "DB error": True, "DB Message": req.ERROR_MSG}
+    return web.json_response(json.loads(json.dumps(data)))
+
+
+async def web_get_a_tiket(request):
+    data = {"Error": False}
+    id = int(request.match_info["id"])
+    if (check_if_everything_is_ok() != True) or (id < 0):
+        data = {"Error": True}
+        return web.json_response(json.loads(json.dumps(data, indent=4)))
+    try:
+        req_str = (
+            "SELECT id, serveur, objet, description, date, heure, utilisateur_emmeteur_du_ticket, date_pec, heure_pec, date_fin, heure_fin, urgence, etat, technicien_affecte, technicien_qui_archive from tickets WHERE id ="
+            + str(id)
+            + ""
+        )
+        req = db_run(req_str)
+
+        for i in req.CONTENT:
+            print(i)
+            ticket_temp = ticket(
+                id=i[0],
+                serveur=i[1],
+                objet=i[2],
+                description=i[3],
+                date=i[4],
+                heure=i[5],
+                utilisateur_emmeteur_du_ticket=i[6],
+                date_pec=i[7],
+                heure_pec=i[8],
+                date_fin=i[9],
+                heure_fin=i[10],
+                urgence=i[11],
+                etat=i[12],
+                technicien_affecte=i[13],
+                technicien_qui_archive=i[14],
+            )
+
+            TEMP = {
+                "id": ticket_temp.id,
+                "serveur": ticket_temp.serveur,
+                "objet": ticket_temp.objet,
+                "description": ticket_temp.description,
+                "date": ticket_temp.date,
+                "heure": ticket_temp.heure,
+                "utilisateur_emmeteur_du_ticket": ticket_temp.utilisateur_emmeteur_du_ticket,
+                "date_pec": ticket_temp.date_pec,
+                "heure_pec": ticket_temp.heure_pec,
+                "date_fin": ticket_temp.date_fin,
+                "heure_fin": ticket_temp.heure_fin,
+                "urgence": ticket_temp.urgence,
+                "etat": ticket_temp.etat,
+                "technicien_affecte": ticket_temp.technicien_affecte,
+                "technicien_qui_archive": ticket_temp.technicien_qui_archive,
+            }
+
+        data = {"Error": False, "Ticket": TEMP}
+
+    except:
+        data = {"Error": True, "DB error": True, "DB Message": req.ERROR_MSG}
     return web.json_response(json.loads(json.dumps(data)))
 
 
@@ -188,13 +247,11 @@ async def web_create_tiket(request):
     data = {"Error": False}
     return web.json_response(json.loads(json.dumps(data)))
 
-async def web_get_a_tiket(request):
-    data = {"Error": False}
-    return web.json_response(json.loads(json.dumps(data)))
 
 async def web_edit_tiket(request):
     data = {"Error": False}
     return web.json_response(json.loads(json.dumps(data)))
+
 
 # Définition des routes
 app = web.Application()
@@ -205,8 +262,8 @@ app.router.add_get("/tickets", web_get_all_tikets)
 app.router.add_post("/tickets", web_create_tiket)
 
 # CRUD
-app.router.add_get("/tickets/ID", web_get_a_tiket)
-app.router.add_post("/tickets/ID", web_edit_tiket)
+app.router.add_get("/tickets/{id}", web_get_a_tiket)
+app.router.add_post("/tickets/{id}", web_edit_tiket)
 
 
 web.run_app(app)
