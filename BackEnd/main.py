226dfbfd07bf61a07d0f6db1722676fbd7eae29f
Nem-developing <nem05yt@gmail.com>
2023-02-26 23:31:59 +0100

    :tada: (Backend) : Add route get all tickets !

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -66,55 +66,40 @@ def db_run(CMD):
     return body_sql(CONTENT=DATA,ERROR=False)
 
 
+def db_ok():
+    OK = False
+    try:
+        REQ = db_run("SELECT * from tickets LIMIT 0")
+        db_status = "Green"
+        db_error = "None"
+
+        if (REQ.ERROR != False):
+            db_status = "Red"
+            db_error = REQ.ERROR_MSG
+    except: 
+        db_status = "Red"
+        db_error =  "Cannot establish connexion with the database"
+        
+    return db_status,db_error
 
 def display():
-    REQ = db_run("SELECT * from tickets LIMIT 0")
-    db_status = "Green"
-    db_error = "None"
-
-    if (REQ.ERROR != False):
-        db_status = "Red"
-        db_error = REQ.ERROR_MSG
-
+    db_status,db_error = db_ok()
     data = {
         "api_version": API_VERSION,
         "status": "running",
         "database": db_status,
         "database_error": db_error,
         "created_by": "Néhémie Barkia",
-        "documentation": "https://github.com/Nem-developing/Fix/wiki",
+        "documentation": "https://github.com/Nem-developing/Fix/wiki"
     }
     return json.dumps(data, indent=4)
 
 
-
-
-# Tickets
-## Get all Tickets
-def get_all_tikets():
-
-    return
-
-## Get one Ticket
-def get_a_tiket():
-
-    return
-
-def create_tiket():
-
-    return
-
-
-def edit_tiket():
-
-    return
-
-
-# Utilistateurs
-
-
-
-
+def check_if_everything_is_ok():
+    db_status,db_error = db_ok()
+    if db_error == True:
+        return False
+    return True
 
 
 
@@ -124,18 +109,46 @@ def edit_tiket():
 
 
 async def index(request):
-    return web.json_response(json.loads(display()))
+    return web.json_response(json.loads())
+
+async def web_get_all_tikets(request):
+    data = {"Error": False}
+    if check_if_everything_is_ok() != True:
+        data = {"Error": True}
+        return web.json_response(json.loads(json.dumps(data, indent=4)))
+    try:
+        req = db_run("SELECT * from tickets")
+        tickets = req.CONTENT
+        data = data = {"Error": False, "Tickets": tickets}
+    except:
+        data = {"Error": True, "DB error": True, "DB Message" : req.ERROR_MSG}
+    
+    return web.json_response(json.loads(json.dumps(data, indent=4)))
+
+
+
+
+async def web_create_tiket(request):
+    return web.json_response(json.loads())
+
+async def web_get_a_tiket(request):
+    return web.json_response(json.loads())
+
+async def web_edit_tiket(request):
+    return web.json_response(json.loads())
 
 
 # Définition des routes
 app = web.Application()
 app.router.add_get("/", index)
 
+# GET
+app.router.add_get("/tickets", web_get_all_tikets)  
+app.router.add_post("/tickets", web_create_tiket)
+
 # CRUD
-app.router.add_get("/tickets", get_all_tikets)
-app.router.add_post("/tickets", create_tiket)
-app.router.add_get("/tickets/ID", get_a_tiket)
-app.router.add_post("/tickets/ID", edit_tiket)
+app.router.add_get("/tickets/ID", web_get_a_tiket)
+app.router.add_post("/tickets/ID", web_edit_tiket)
 
 
 
