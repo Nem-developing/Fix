445e71168ca3582ff7c7ef9362175f355283a954
Nem-developing <nem05yt@gmail.com>
2025-04-29 12:26:21 +0200

    :wrench: HOTFIX - Correction du dockercompose

--- BackEnd/variables/db_config.py
+++ BackEnd/variables/db_config.py
@@ -1,4 +1,24 @@
-DB_HOST = "192.168.1.69"  # Ex : 127.0.0.1
-DB_NAME = "exampledb"  # Ex : Fix
-DB_USER = "exampleuser"  # Ex : nehemie
-DB_PASSORD = "examplepass"  # Ex : B@rki@
+import os
+we_exit=False
+## Récupération des variables et surcharges
+if os.getenv("DB_HOSTNAME") is None:
+    print("DB_HOSTNAME not found in environment variables, using default value")
+    we_exit=True
+if os.getenv("DB_NAME") is None:
+    print("DB_NAME not found in environment variables, using default value")
+    we_exit=True
+if os.getenv("DB_USER") is None:
+    print("DB_USER not found in environment variables, using default value")
+    we_exit=True
+if os.getenv("DB_PASSWORD") is None:
+    print("DB_PASSWORD not found in environment variables, using default value")
+    we_exit=True
+
+if (we_exit):
+    exit(1)
+
+## Affectation des variables
+DB_HOST = os.getenv("DB_HOSTNAME")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSORD = os.getenv("DB_PASSWORD")
