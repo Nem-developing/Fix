86f589022ac2ed24f515e8953c3f8a339fb29064
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2025-06-20 13:26:02 +0200

    WIP Modification du rangement du CSS

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./reset.css"
-import "./index.css"; 
+import "./styles/reset.css";
+import "./styles/index.css";
+import "./styles/pages.css";
+import "./styles/global.css";
+import "./styles/components.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
